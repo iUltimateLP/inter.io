4df4fc1cf34ee80e20012d0516dc767d82aa0b77
Johnny Verbeek <iredcraft@gmail.com>
2019-08-23 17:03:21 +0200

    Added packages styling

--- assets/js/choosePackage.js
+++ assets/js/choosePackage.js
@@ -46,3 +46,46 @@ packages.handlePackageClicked = function(index) {
     document.getElementById("package-info-container-arrow").style.left = arrowOffset + "px";
 }
 
+packages.getContrastCheckedColor = function(hex) {	
+    /*
+        From this W3C document: http://www.webmasterworld.com/r.cgi?f=88&d=9769&url=http://www.w3.org/TR/AERT#color-contrast
+
+        Color brightness is determined by the following formula: 
+        ((Red value X 299) + (Green value X 587) + (Blue value X 114)) / 1000
+    */
+
+    // About half of 256. Lower threshold equals more dark text on dark background
+    threshold = 130; 
+
+    // Helper functions
+    function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+    function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+    function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+    function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
+
+    // Get the hex color amounts
+    hRed = hexToR(hex);
+    hGreen = hexToG(hex);
+    hBlue = hexToB(hex);
+
+    // Calculate the brightness
+    var cBrightness = ((hRed * 299) + (hGreen * 587) + (hBlue * 114)) / 1000;
+
+    // Check the brightness
+    if (cBrightness > threshold){
+        return "dark";
+    } else { 
+        return "light";
+    }	
+}
+
+packages.selectColorOptions = function(index) {
+    var colors = document.getElementsByClassName("package-color");
+    for (var i = 0; i < colors.length; i++) {
+        var checkmark = colors.item(i).children[0];
+        
+        checkmark.classList.remove("selected");
+    }
+
+    colors.item(index).children[0].classList.add("selected");
+}
