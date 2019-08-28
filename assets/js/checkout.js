5c11ade8209b9e1a0eb7164225268ae3939f473b
Johnny Verbeek <iredcraft@gmail.com>
2019-08-29 00:28:03 +0200

    Checkout and payment

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -10,9 +10,55 @@ var checkout = {};
 checkout.currentWizardStep = 0;
 
 checkout.nextWizardStep = function() {
-    checkout.currentWizardStep++;
+    if (checkout.currentWizardStep + 1 <= 2) {
+        checkout.currentWizardStep++;
 
-    var wizardContainer = document.getElementsByClassName("checkout-wizard-container").item(0);
+        for (var i = 0; i < 3; i++) {
+            document.getElementById("checkout-wizard-" + (i + 1)).classList.add("noheight");
+        }
+        document.getElementById("checkout-wizard-" + (checkout.currentWizardStep + 1)).classList.remove("noheight");
 
-    wizardContainer.scrollTo(wizardContainer.getBoundingClientRect().width * checkout.currentWizardStep, 0);
+        var wizardContainer = document.getElementsByClassName("checkout-wizard-container").item(0);
+
+        wizardContainer.scrollTo(wizardContainer.getBoundingClientRect().width * checkout.currentWizardStep, 0);
+    }
+
+    if (checkout.currentWizardStep < 2) {
+        document.getElementById("checkout-wizard-next").classList.remove("disabled");
+    } else {
+        document.getElementById("checkout-wizard-next").classList.add("disabled");
+    }
+
+    if (checkout.currentWizardStep > 0) {
+        document.getElementById("checkout-wizard-prev").classList.remove("disabled");
+    } else {
+        document.getElementById("checkout-wizard-prev").classList.add("disabled");
+    }
+}
+
+checkout.previousWizardStep = function() {
+    if (checkout.currentWizardStep - 1 >= 0) {
+        checkout.currentWizardStep--;
+
+        for (var i = 0; i < 3; i++) {
+            document.getElementById("checkout-wizard-" + (i + 1)).classList.add("noheight");
+        }
+        document.getElementById("checkout-wizard-" + (checkout.currentWizardStep + 1)).classList.remove("noheight");
+
+        var wizardContainer = document.getElementsByClassName("checkout-wizard-container").item(0);
+
+        wizardContainer.scrollTo(wizardContainer.getBoundingClientRect().width * checkout.currentWizardStep, 0);
+    }
+
+    if (checkout.currentWizardStep < 2) {
+        document.getElementById("checkout-wizard-next").classList.remove("disabled");
+    } else {
+        document.getElementById("checkout-wizard-next").classList.add("disabled");
+    }
+
+    if (checkout.currentWizardStep > 0) {
+        document.getElementById("checkout-wizard-prev").classList.remove("disabled");
+    } else {
+        document.getElementById("checkout-wizard-prev").classList.add("disabled");
+    }
 }
